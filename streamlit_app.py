3f4b21a874b7b88c996a0846c394b305175debe0
nunogilsilva <161771971+nunogilsilva@users.noreply.github.com>
2025-09-19 17:25:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     label='Choose up to 5 ingredients:'
@@ -37,6 +37,10 @@ if ingredients_list and name_on_order:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
@@ -53,9 +57,9 @@ if ingredients_list and name_on_order:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/all")
+#smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/all")
 
-for fruit in smoothiefroot_response.json():
-  my_update_stmt = f" update smoothies.public.FRUIT_OPTIONS set SEARCH_ON = '{fruit['name']}' WHERE fruit_name = '{fruit['name']}' "
-  session.sql(my_update_stmt).collect()
+#for fruit in smoothiefroot_response.json():
+  #my_update_stmt = f" update smoothies.public.FRUIT_OPTIONS set SEARCH_ON = '{fruit['name']}' WHERE fruit_name = '{fruit['name']}' "
+  #session.sql(my_update_stmt).collect()
 
